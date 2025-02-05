0f5a7bcf4f1c5102624f3338d90e1852a6687ac8
Abel José <abeljose018@gmail.com>
2025-02-04 21:46:20 -0300

    \\

--- Interfaces_ControleSerial.c
+++ Interfaces_ControleSerial.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
+#include <ctype.h>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
-#include "hardware/uart.h"
-#include <ctype.h>
 #include "pio_matrix.pio.h"
 #include "./Bibliotecas/led_matriz.h"
 #include "./Bibliotecas/botao.h"
 
 #define pin_blue 12
 #define pin_green 11
-
-#define UART_ID uart0
-#define BAUD_RATE 115200
-#define UART_TX_PIN 0
-#define UART_RX_PIN 1
 
 void inicializar_pinos(){
     gpio_init(pin_blue);
@@ -33,10 +27,6 @@ int main()
 
     inicializar_pinos();
     inicializar_botao();
-
-    uart_init(UART_ID, BAUD_RATE);
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
     
     stdio_init_all();
     
